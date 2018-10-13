1ee4519b0caebd12fdb468359cb2e0d970796f16
devlinpadayachee@gmail.com <devlinpadayachee@gmail.com>
2018-10-13 18:07:47 +0200

    Added plugins directory

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# mtnSubscriptions: Application developed for MTN to manage subscriber subscriptions
+# Application for managing subscriber subscriptions
+
+##Usage
+
+```javascript
+
+var plugins = {
+	'elapsed-time':{}
+};
+
+server.pack.events.on('log', function (event, tags){
+	if (tags.elapsed) {
+		//do something with the elapsed event
+		console.log('elapsed, event);
+	}
+});
+
+server.pack.require(plugins, function (err) {
+	if (err) {throw err;}
+	// plugin required, start server
+});
+```
--- index.js
+++ index.js
@@ -3,38 +3,47 @@
 const Hapi = require('hapi');
 
 const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
+  port: 3000,
+  host: 'localhost'
 });
 
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-
-        return 'Hello, world!';
-    }
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => {
+    //request.log(['a', 'b'], 'Request into hello world')
+    request.logger.info('In handler %s', request.path);
+    return 'Hello, world!';
+  }
 });
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: (request, h) => {
-
-        return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
-    }
-});
+// server.route({
+//     method: 'GET',
+//     path: '/{name}',
+//     handler: (request, h) => {
+//
+//         return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
+//     }
+// });
 
 const init = async () => {
 
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.register({
+    plugin: require('hapi-pino'),
+      options: {
+        prettyPrint: true,
+        logEvents: ['response']
+      }
+    });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
 
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
